f11a1cb83e4c49b416f16ac9dba8e01bc17b5f7c
Adrian Mejia <admejiar@cisco.com>
2017-08-16 18:46:08 -0400

    add flag to avoid HTTP issue

--- test/e2e/gdax.http.mock.js
+++ test/e2e/gdax.http.mock.js
@@ -3,19 +3,24 @@ const https = require('https');
 const fs = require('fs');
 const debug = require('debug')('crybot:mock:http');
 
-
 const { accounts } = require('../responses/gdax');
 
 // curl - k https://localhost:7777/accounts
 // https://www.akadia.com/services/ssh_test_certificate.html
+// to avoid "unable to verify the first certificate" use NODE_TLS_REJECT_UNAUTHORIZED=0
+// use NODE_TLS_REJECT_UNAUTHORIZED=0 --- https://github.com/request/request/issues/2061
 var options = {
   key: fs.readFileSync('/Users/admejiar/scripts/certs/localhost/server.key'),
   cert: fs.readFileSync('/Users/admejiar/scripts/certs/localhost/server.crt'),
-  // requestCert: false,
-  // rejectUnauthorized: false
-  ciphers: 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES256-SHA384',
-  honorCipherOrder: true,
-  secureProtocol: 'TLSv1_2_method'
+  requestCert: false,
+  rejectUnauthorized: false
+
+  // ciphers: 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES256-SHA384',
+  // honorCipherOrder: true,
+  // secureProtocol: 'TLSv1_2_method'
+
+  // requestCert: true,
+  // rejectUnauthorized: true
 };
 
 class GdaxHttpMock {
@@ -37,6 +42,8 @@ class GdaxHttpMock {
       }).listen(port, () => {
         debug(`HTTP listening on ${port}`);
         resolve(port);
+      }).on('close', () => {
+        debug('Closing HTTP server');
       });
     });
   }
